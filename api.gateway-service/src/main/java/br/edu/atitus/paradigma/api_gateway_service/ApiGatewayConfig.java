bee6b119917f89beb67b9dfc0c1ece2c4ea5b0be
Carlos Henrique (Icarus) <167153188+IcarusDV@users.noreply.github.com>
2024-11-21 12:47:00 -0300

    Update ApiGatewayConfig.java

--- api.gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/ApiGatewayConfig.java
+++ api.gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/ApiGatewayConfig.java
@@ -11,21 +11,15 @@ public class ApiGatewayConfig {
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-               
-                .route("saudacao_service", r -> r.path("/saudacao-service/**")
-                        
-                        .uri("http://localhost:8081"))
-                
-                
                 .route("cambio_service", r -> r.path("/cambio-service/**")
-                        
+                        .filters(f -> f.addRequestHeader("Usuario", "Carlos Henrique Ferrão"))
                         .uri("http://localhost:8100"))
-                
-                
                 .route("produto_service", r -> r.path("/produto-service/**")
-                        
+                        .filters(f -> f.addRequestHeader("Usuario", "Carlos Henrique Ferrão"))
                         .uri("http://localhost:8200"))
-                
+                .route("saudacao_service", r -> r.path("/saudacao-service/**")
+                        .filters(f -> f.addRequestHeader("Usuario", "Carlos Henrique Ferrão"))
+                        .uri("http://localhost:8081"))
                 .build();
     }
 }
